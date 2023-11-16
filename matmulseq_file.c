ce73a6d82586b6531ecca552d6fa283447bc056f
yjmantilla <yjmantilla@gmail.com>
2023-11-16 11:01:41 -0500

    just spaces

--- matmulseq_file.c
+++ matmulseq_file.c
@@ -7,89 +7,89 @@ double **a, **b, **c;
 int matrixSize;
 
 double **allocateMatrix() {
-	int i;
-	double *vals, **temp;
+    int i;
+    double *vals, **temp;
 
-	// allocate space for values of a matrix
-	vals = (double *) malloc (matrixSize * matrixSize * sizeof(double));
+    // allocate space for values of a matrix
+    vals = (double *) malloc (matrixSize * matrixSize * sizeof(double));
 
-	// allocate vector of pointers to create the 2D array
-	temp = (double **) malloc (matrixSize * sizeof(double*));
+    // allocate vector of pointers to create the 2D array
+    temp = (double **) malloc (matrixSize * sizeof(double*));
 
-	for(i=0; i < matrixSize; i++)
-		temp[i] = &(vals[i * matrixSize]);
+    for(i=0; i < matrixSize; i++)
+        temp[i] = &(vals[i * matrixSize]);
 
-	return temp;
+    return temp;
 }
 
 void mm(void) {
-	int i,j,k;
-	double sum;
-	// matrix multiplication
-	for (i = 0; i < matrixSize; i++) {
-		for (j = 0; j < matrixSize; j++) {
-			sum = 0.0;
-			// dot product
-			for (k = 0; k < matrixSize; k++) {
-				sum = sum + a[i][k] * b[k][j];
-			}
-			c[i][j] = sum;
-		}
-	}
+    int i,j,k;
+    double sum;
+    // matrix multiplication
+    for (i = 0; i < matrixSize; i++) {
+        for (j = 0; j < matrixSize; j++) {
+            sum = 0.0;
+            // dot product
+            for (k = 0; k < matrixSize; k++) {
+                sum = sum + a[i][k] * b[k][j];
+            }
+            c[i][j] = sum;
+        }
+    }
 }
 void printResult(void){
-	int i, j;	
-	for(i=0;i<matrixSize;i++){
-		for(j=0;j<matrixSize;j++){
-			printf("%lf ", c[i][j]);
-		}
-		printf("\n");
-	}
+    int i, j;    
+    for(i=0;i<matrixSize;i++){
+        for(j=0;j<matrixSize;j++){
+            printf("%lf ", c[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main(void) {
-	int i, j, k;
-	int nmats;
-	char *fname = "matrices_dev.dat"; //Change to matrices_large.dat for performance evaluation
-	FILE *fh;
+    int i, j, k;
+    int nmats;
+    char *fname = "matrices_dev.dat"; //Change to matrices_large.dat for performance evaluation
+    FILE *fh;
 
-	printf("Start\n");
-	fh = fopen(fname, "r");
-	//First line indicates how many pairs of matrices there are and the matrix size
-	fscanf(fh, "%d %d\n", &nmats, &matrixSize);
+    printf("Start\n");
+    fh = fopen(fname, "r");
+    //First line indicates how many pairs of matrices there are and the matrix size
+    fscanf(fh, "%d %d\n", &nmats, &matrixSize);
 
-	//Dynamically create matrices of the size needed
-	a = allocateMatrix();
-	b = allocateMatrix();
-	c = allocateMatrix();
+    //Dynamically create matrices of the size needed
+    a = allocateMatrix();
+    b = allocateMatrix();
+    c = allocateMatrix();
 
-	printf("Loading %d pairs of square matrices of size %d from %s...\n", nmats, matrixSize, fname);
-	for(k=0;k<nmats;k++){
-		for(i=0;i<matrixSize;i++){
-			for(j=0;j<matrixSize;j++){
-				fscanf(fh, "%lf", &a[i][j]);
-			}
-		}
-		for(i=0;i<matrixSize;i++){
-			for(j=0;j<matrixSize;j++){
-				fscanf(fh, "%lf", &b[i][j]);
-			}
-		}
+    printf("Loading %d pairs of square matrices of size %d from %s...\n", nmats, matrixSize, fname);
+    for(k=0;k<nmats;k++){
+        for(i=0;i<matrixSize;i++){
+            for(j=0;j<matrixSize;j++){
+                fscanf(fh, "%lf", &a[i][j]);
+            }
+        }
+        for(i=0;i<matrixSize;i++){
+            for(j=0;j<matrixSize;j++){
+                fscanf(fh, "%lf", &b[i][j]);
+            }
+        }
 
-		printf("Multiplying two matrices...\n"); //Remove this line for performance tests
-		mm();
-		printResult(); //Remove this line for performance tests
-	}
-	fclose(fh);
+        printf("Multiplying two matrices...\n"); //Remove this line for performance tests
+        mm();
+        printResult(); //Remove this line for performance tests
+    }
+    fclose(fh);
 
-	// Free memory
-	free(*a);
-	free(a);
-	free(*b);
-	free(b);
-	free(*c);
-	free(c);
-	printf("Done.\n");
-	return 0;
+    // Free memory
+    free(*a);
+    free(a);
+    free(*b);
+    free(b);
+    free(*c);
+    free(c);
+    printf("Done.\n");
+    return 0;
 }
 
